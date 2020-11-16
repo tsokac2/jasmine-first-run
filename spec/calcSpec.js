7890728dec5d5a0383d31e734cc5660a58fe31ed
tsokac2 <tsokac2@gmail.com>
2020-11-16 07:30:22 +0000

    Adding spyOn test to calcSpec.js

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -3,5 +3,13 @@ describe("Calculator", function(){
         it("should return 42", function(){
             expect(addition(20, 22)).toBe(42);
         });
+        it("should return 26", function(){
+            expect(addition(7, 19)).toBe(26);
+        });
+        it("shoud tetur an error if we don't supply two numbers", function(){
+            spyOn(window, "alert");
+            addition("Hitchhikers", "Guide");
+            expect(window.alert).toHaveBeenCalledWith ("Error!");
+        });
     });
 });
